542b7143edfee8a63ce528a757fa2b2911975bc1
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2024-04-16 11:25:08 +0530

    fixed routes issue

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/orderform" element={<Orderform />} />
         <Route path="/shop" element={<Shop />} />
-        <Route path="shopdetails" element={<Shopdetails />} />
+        <Route path="/shopdetails" element={<Shopdetails />} />
 
 
 
