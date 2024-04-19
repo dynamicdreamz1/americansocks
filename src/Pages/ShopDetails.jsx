fd286316b9d2b759e7004533c73cf4dd016274e7
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2024-04-19 13:29:01 +0530

    remove category images

--- src/Pages/ShopDetails.jsx
+++ src/Pages/ShopDetails.jsx
@@ -2,55 +2,37 @@ import React, { useEffect, useState } from 'react'
 import Productdeatils from "../Component/Productdeatils"
 import Relatedproductslider from "../Component/Relatedproductslider"
 import { useLocation } from 'react-router-dom';
-import { getProductVariationsList, productList, relatedProductListApi } from '../services/shop';
+import { getProductVariationsList, relatedProductListApi } from '../services/shop';
 
 function ShopDetailMain() {
   const location = useLocation();
-  const product = location.state.product
-  const [variationsList, setVariationsList] = useState([])
-  const [relatedProductList, setRelatedProductList] = useState([])
-  const [singleProduct, setsingleProduct] = useState()
-
+  const product = location.state.product;
+  const [variationsList, setVariationsList] = useState([]);
+  const [relatedProductList, setRelatedProductList] = useState([]);
+  const [singleProduct, setSingleProduct] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await getProductVariationsList(product.id);
-      setVariationsList(result)
+      const [variations, relatedProducts, singleProd] = await Promise.all([
+        getProductVariationsList(product.id),
+        relatedProductListApi(100, { include: product.related_ids }),
+        relatedProductListApi(100, { include: [product.id] })
+      ]);
+      setVariationsList(variations);
+      setRelatedProductList(relatedProducts);
+      setSingleProduct(singleProd[0]);
     };
 
     fetchData();
 
-
-    const fetchRelatedProduct = async () => {
-      const result = await relatedProductListApi(100, { include: product.related_ids });
-      setRelatedProductList(result)
-    };
-
-    fetchRelatedProduct();
-
-    const getSingleProduct = async () => {
-      const result = await relatedProductListApi(100, { include: [product.id] });
-      
-      setsingleProduct(result[0])
-    };
-
-    getSingleProduct();
-
-  }, [product])
-
-
-  console.log("singleProduct",singleProduct);
-
-
+  }, [product]);
 
   return (
     <>
-   {singleProduct ?   <Productdeatils product={singleProduct} variationsList={variationsList} /> : ""} 
+      {singleProduct && <Productdeatils product={singleProduct} variationsList={variationsList} />} 
       <Relatedproductslider relatedProductList={relatedProductList} /> 
-      <></>
     </>
-
   )
 }
 
-export default ShopDetailMain
+export default ShopDetailMain;
