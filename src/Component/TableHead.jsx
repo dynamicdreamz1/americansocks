b8186c6dc168d5f64dd74bec256a4a6704989123
dynamicdreamz1 <programmer121.dynamicdreamz@gmail.com>
2024-09-09 13:20:03 +0530

    fixed show label

--- src/Component/TableHead.jsx
+++ src/Component/TableHead.jsx
@@ -7,19 +7,6 @@ const TableHeader = ({ variationData }) => {
         {variationData.length > 0 && variationData.map((size, index) => (
           <th key={index}>
             {size?.attributes?.attribute_pa_size?.toUpperCase()}
-            {/* {size?.is_pre_order === "yes" && (
-              <div style={{
-                display: 'inline-block',
-                backgroundColor: '#01426a',
-                color: 'white',
-                fontSize: 'smaller',
-                padding: '2px 5px',
-                marginLeft: '5px',
-                borderRadius: '3px'
-              }}>
-                Pre-Order
-              </div>
-            )} */}
           </th>
         ))}
       </tr>
